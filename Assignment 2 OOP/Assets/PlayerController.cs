44b6d66323c535db50563baa97c1e30a009e61ac
Jiachen Tang <tjc9606ok@gmail.com>
2022-09-27 02:56:10 -0400

    2d player controller failed, use 3d game objects

---
+++ Assignment 2 OOP/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 3;
+
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        characterController.Move(move * Time.deltaTime * playerSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Block")
+        {
+            Debug.Log("Block caught");
+            other.gameObject.SetActive(false);
+        }
+    }
+}
